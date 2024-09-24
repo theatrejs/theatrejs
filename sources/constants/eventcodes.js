a762159ffedea1a147632921479a2f6756e8dace
Alexandre Blondeau <deformhead@gmail.com>
2024-09-24 08:33:48 +0200

    add 'playstation' gamepad event codes

--- sources/constants/eventcodes.js
+++ sources/constants/eventcodes.js
@@ -1,9 +1,11 @@
+import * as GAMEPADPLAYSTATION from './eventcodes/gamepad.playstation.js';
 import * as GAMEPADXBOX from './eventcodes/gamepad.xbox.js';
 import * as KEYBOARDAZERTY from './eventcodes/keyboard.azerty.js';
 import * as KEYBOARDQWERTY from './eventcodes/keyboard.qwerty.js';
 
 export {
 
+    GAMEPADPLAYSTATION,
     GAMEPADXBOX,
     KEYBOARDAZERTY,
     KEYBOARDQWERTY
---
+++ sources/constants/eventcodes/gamepad.playstation.js
@@ -0,0 +1,304 @@
+/**
+ * @module EVENTCODES/GAMEPADPLAYSTATION
+ */
+
+/**
+ * The PLAYSTATION gamepad 'CROSS' event code.
+ * @type {'ClusterRightButtonBottom'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const CROSS = 'ClusterRightButtonBottom';
+
+/**
+ * The PLAYSTATION gamepad 'CIRCLE' event code.
+ * @type {'ClusterRightButtonRight'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const CIRCLE = 'ClusterRightButtonRight';
+
+/**
+ * The PLAYSTATION gamepad 'SQUARE' event code.
+ * @type {'ClusterRightButtonLeft'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const SQUARE = 'ClusterRightButtonLeft';
+
+/**
+ * The PLAYSTATION gamepad 'TRIANGLE' event code.
+ * @type {'ClusterRightButtonTop'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const TRIANGLE = 'ClusterRightButtonTop';
+
+/**
+ * The PLAYSTATION gamepad 'L1' event code.
+ * @type {'ClusterFrontButtonTopLeft'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const L1 = 'ClusterFrontButtonTopLeft';
+
+/**
+ * The PLAYSTATION gamepad 'R1' event code.
+ * @type {'ClusterFrontButtonTopRight'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const R1 = 'ClusterFrontButtonTopRight';
+
+/**
+ * The PLAYSTATION gamepad 'L2' event code.
+ * @type {'ClusterFrontButtonBottomLeft'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const L2 = 'ClusterFrontButtonBottomLeft';
+
+/**
+ * The PLAYSTATION gamepad 'R2' event code.
+ * @type {'ClusterFrontButtonBottomRight'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const R2 = 'ClusterFrontButtonBottomRight';
+
+/**
+ * The PLAYSTATION gamepad 'SELECT' event code.
+ * @type {'ClusterCenterButtonLeft'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const SELECT = 'ClusterCenterButtonLeft';
+
+/**
+ * The PLAYSTATION gamepad 'START' event code.
+ * @type {'ClusterCenterButtonRight'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const START = 'ClusterCenterButtonRight';
+
+/**
+ * The PLAYSTATION gamepad 'L3' event code.
+ * @type {'StickLeftButton'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const L3 = 'StickLeftButton';
+
+/**
+ * The PLAYSTATION gamepad 'R3' event code.
+ * @type {'StickRightButton'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const R3 = 'StickRightButton';
+
+/**
+ * The PLAYSTATION gamepad 'UP' event code.
+ * @type {'ClusterLeftButtonTop'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const UP = 'ClusterLeftButtonTop';
+
+/**
+ * The PLAYSTATION gamepad 'DOWN' event code.
+ * @type {'ClusterLeftButtonBottom'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const DOWN = 'ClusterLeftButtonBottom';
+
+/**
+ * The PLAYSTATION gamepad 'LEFT' event code.
+ * @type {'ClusterLeftButtonLeft'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const LEFT = 'ClusterLeftButtonLeft';
+
+/**
+ * The PLAYSTATION gamepad 'RIGHT' event code.
+ * @type {'ClusterLeftButtonRight'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const RIGHT = 'ClusterLeftButtonRight';
+
+/**
+ * The PLAYSTATION gamepad 'PLAYSTATION' event code.
+ * @type {'ClusterCenterButtonCenter'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const PLAYSTATION = 'ClusterCenterButtonCenter';
+
+/**
+ * The PLAYSTATION gamepad 'LSLEFT' event code.
+ * @type {'StickLeftLeft'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const LSLEFT = 'StickLeftLeft';
+
+/**
+ * The PLAYSTATION gamepad 'LSRIGHT' event code.
+ * @type {'StickLeftRight'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const LSRIGHT = 'StickLeftRight';
+
+/**
+ * The PLAYSTATION gamepad 'LSUP' event code.
+ * @type {'StickLeftUp'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const LSUP = 'StickLeftUp';
+
+/**
+ * The PLAYSTATION gamepad 'LSDOWN' event code.
+ * @type {'StickLeftDown'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const LSDOWN = 'StickLeftDown';
+
+/**
+ * The PLAYSTATION gamepad 'RSLEFT' event code.
+ * @type {'StickRightLeft'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const RSLEFT = 'StickRightLeft';
+
+/**
+ * The PLAYSTATION gamepad 'RSRIGHT' event code.
+ * @type {'StickRightRight'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const RSRIGHT = 'StickRightRight';
+
+/**
+ * The PLAYSTATION gamepad 'RSUP' event code.
+ * @type {'StickRightUp'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const RSUP = 'StickRightUp';
+
+/**
+ * The PLAYSTATION gamepad 'RSDOWN' event code.
+ * @type {'StickRightDown'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const RSDOWN = 'StickRightDown';
+
+/**
+ * The PLAYSTATION gamepad 'CONNECTED' event code.
+ * @type {'Connected'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const CONNECTED = 'Connected';
+
+/**
+ * The PLAYSTATION gamepad 'DISCONNECTED' event code.
+ * @type {'Disconnected'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const DISCONNECTED = 'Disconnected';
+
+/**
+ * The PLAYSTATION gamepad 'VIBRATEEND' event code.
+ * @type {'VibrateEnd'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const VIBRATEEND = 'VibrateEnd';
+
+/**
+ * The PLAYSTATION gamepad 'VIBRATESTART' event code.
+ * @type {'VibrateStart'}
+ * @constant
+ *
+ * @memberof module:EVENTCODES/GAMEPADPLAYSTATION
+ */
+const VIBRATESTART = 'VibrateStart';
+
+export {
+
+    CROSS,
+    CIRCLE,
+    SQUARE,
+    TRIANGLE,
+
+    L1,
+    R1,
+    L2,
+    R2,
+
+    SELECT,
+    START,
+
+    L3,
+    R3,
+
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+
+    PLAYSTATION,
+
+    LSLEFT,
+    LSRIGHT,
+    LSUP,
+    LSDOWN,
+    RSLEFT,
+    RSRIGHT,
+    RSUP,
+    RSDOWN,
+
+    CONNECTED,
+    DISCONNECTED,
+    VIBRATEEND,
+    VIBRATESTART
+};
