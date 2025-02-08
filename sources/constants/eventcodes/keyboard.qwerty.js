8d024293a5b3ed176b035c21580f18b9a980193c
Alexandre Blondeau <deformhead@gmail.com>
2025-02-08 23:40:07 +0100

    clean source code

--- sources/constants/eventcodes/keyboard.qwerty.js
+++ sources/constants/eventcodes/keyboard.qwerty.js
@@ -1,10 +1,10 @@
-import {DIGIT_0, DIGIT_1, DIGIT_2, DIGIT_3, DIGIT_4, DIGIT_5, DIGIT_6, DIGIT_7, DIGIT_8, DIGIT_9, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, SPACE} from './keyboard.qwertycharacters.js';
-import {ENTER, DELETE} from './keyboard.qwertyeditors.js';
-import {F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12} from './keyboard.qwertyfunctions.js';
-import {CAPSLOCK} from './keyboard.qwertylockers.js';
-import {ALT_LEFT, ALT_RIGHT, CONTROL_LEFT, CONTROL_RIGHT, SHIFT_LEFT, SHIFT_RIGHT} from './keyboard.qwertymodifiers.js';
-import {TAB, UP, LEFT, RIGHT, DOWN} from './keyboard.qwertynavigators.js';
-import {CONTEXTMENU, ESCAPE} from './keyboard.qwertysystems.js';
+import {DIGIT_0, DIGIT_1, DIGIT_2, DIGIT_3, DIGIT_4, DIGIT_5, DIGIT_6, DIGIT_7, DIGIT_8, DIGIT_9, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, SPACE} from './keyboard.qwerty-characters.js';
+import {ENTER, DELETE} from './keyboard.qwerty-editors.js';
+import {F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12} from './keyboard.qwerty-functions.js';
+import {CAPSLOCK} from './keyboard.qwerty-lockers.js';
+import {ALT_LEFT, ALT_RIGHT, CONTROL_LEFT, CONTROL_RIGHT, SHIFT_LEFT, SHIFT_RIGHT} from './keyboard.qwerty-modifiers.js';
+import {TAB, UP, LEFT, RIGHT, DOWN} from './keyboard.qwerty-navigators.js';
+import {CONTEXTMENU, ESCAPE} from './keyboard.qwerty-systems.js';
 
 /**
  * @module EVENTCODES/KEYBOARDS/QWERTY
